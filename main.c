d2a6126091fb1d1893da1aaff448bb9210b3432f
Phyopma <phyopyaemaung1@gmail.com>
2025-01-22 00:02:50 -0800

    initial commit; setting up basics for clox

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "chunk.h"
+#include "debug.h"
+#include "common.h"
+
+int main(int argc, char const *argv[])
+{
+    Chunk chunk;
+    initChunk(&chunk);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
+}
