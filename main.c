fa4b2b35223d9b26db1616ffa125a268de910633
Phyopma <phyopyaemaung1@gmail.com>
2025-06-25 23:24:19 -0700

    fixed bugs in scanToken

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-static void repl() {
+static void repl()
+{
     char line[1024];
-    for (;;) {
+    for (;;)
+    {
         printf("> ");
 
-        if (!fgets(line, sizeof(line), stdin)) {
+        if (!fgets(line, sizeof(line), stdin))
+        {
             printf("\n");
             break;
         }
@@ -21,23 +23,12 @@ static void repl() {
     }
 }
 
-
-static void runFile(const char *path) {
-    char *source = readFile(path);
-    InterpretResult result = interpret(source);
-    free(source);
-
-    if (result == INTERPRET_COMPILE_ERROR)
-        exit(65);
-    if (result == INTERPRET_RUNTIME_ERROR)
-        exit(70);
-}
-
-
-static char* readFile(const char *path) {
+static char *readFile(const char *path)
+{
     FILE *file = fopen(path, "rb");
 
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "Could not open file \"%s\".\n", path);
         exit(74);
     }
@@ -48,14 +39,16 @@ static char* readFile(const char *path) {
 
     char *buffer = (char *)malloc(fileSize + 1);
 
-    if (buffer == NULL) {
+    if (buffer == NULL)
+    {
         fprintf(stderr, "Not enough memomry to read \"%s\".\n", path);
         exit(74);
     }
 
     size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
 
-    if (bytesRead < fileSize) {
+    if (bytesRead < fileSize)
+    {
         fprintf(stderr, "Could not read file \"%s\".\n", path);
         exit(74);
     }
@@ -64,6 +57,18 @@ static char* readFile(const char *path) {
 
     fclose(file);
     return buffer;
+}
+
+static void runFile(const char *path)
+{
+    char *source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR)
+        exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR)
+        exit(70);
 }
 
 int main(int argc, char const *argv[])
@@ -81,16 +86,21 @@ int main(int argc, char const *argv[])
     // writeChunk(&chunk, OP_NEGATE, 123);
     // writeChunk(&chunk, OP_RETURN, 123);
 
-    if (argc == 1) {
+    if (argc == 1)
+    {
         repl();
-    } else if (argc == 2) {
+    }
+    else if (argc == 2)
+    {
         runFile(argv[1]);
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Usage: clox [path]\n");
         exit(64);
     }
 
-    interpret(&chunk);
+    // interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
     return 0;
